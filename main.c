111219830504c04644f1f165c2377fe583c71177
Francileudo Oliveira <83087997+fransilva0@users.noreply.github.com>
2022-03-08 09:25:36 -0300

    organizando a estruturação inicial do projeto

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 typedef struct{
 
-char resposta[30];
+char resposta[50];
 struct Lista *prox;
 
 } Lista;
@@ -14,33 +14,41 @@ Lista * criar(){
 return NULL;
 }
 
-Lista * insere (Lista *l, char v){
-Lista *novo;
-  
-novo = (Lista *) malloc(sizeof(Lista));
-novo->resposta[30] = v;
-novo->prox = (struct Lista *) l;
+Lista * insere (Lista *lista, char valor){
+
+Lista * novo = (Lista *) malloc(sizeof(Lista));
+novo->resposta[50] = valor;
+novo->prox = (struct Lista *) lista;
 return novo;
 }
 
 // Main do Projeto 
 
+void apresentar(Lista * lista){
+  Lista * p;
+  p=lista;
+  while(p!=NULL){
+    printf("Valor: %s\n",&p->resposta[50]);
+    p = (Lista *) p->prox;
+  }  
+}
+
 int main(void) {
-	char *perguntas[2] = {"pergunta 1", "pergunta 2"};
+	char *perguntas[5] = {"Qual o seu nome?", "Você conhece os protudos da Empresa?", "Como você avalia o atendimento prestado pela nossa equipe durante a compra de alguns dos produtos?", "Os nossos produtos estão abaixo, acima, ou atende às suas expectativas?","Em uma escala de 0 a 10, o quanto você nos recomendaria para um amigo ou familiar?"};
   Lista *lista;
-  char valor[30];
+  char valor[50];
 	
 	printf("Ola, a empresa Start esta passando por melhorias e precisa da sua opiniao!\n");
 	printf("Por favor, responda as seguintes perguntas com o maximo de sinseridade possivel.\n");
 
   lista = criar();
 
-  for(int i = 0; i <= 2; i++){
+  for(int i = 0; i <= 4; i++){
     printf("%s\n", perguntas[i]);
-    scanf("%s",&valor[30]);
+    scanf("%s",&valor[50]);
     lista = insere(lista,*valor);
   }
 
-  
+  apresentar(lista);
   return 0;
 }
