c3a90b0adc890cc562d0323b3ace6e28c58ad27f
Tharcio Camboim <89159550+TharcioCBM@users.noreply.github.com>
2022-03-10 03:50:11 -0300

    Atualização do código do projeto de programação 1 by Tharcio Camboim UEPB Patos
    Adicionado o parâmetro "size" na função "(Lista * insere)".
    O parâmetro "lista" da função "insere" agora é chamado de "inicio".
    Alterado o modelo de inserção de informações na função "insere", agora funcional para o tipo "char".
    Refeito o modelo de criação da lista encadeada, agora os novos nós serão inseridos ao final da lista.
    Atualizada a função "apresentar", agora ela utiliza "do{}while();" para repetição.
    Remoção do Modificador (Lista *) da função "apresentar".
    Alterado o nome da variável "char valor;" agora se chama "RecebeResposta".
    Alterada a função de entrada scanf para fgets, agora espaços vazios também serão armazenados.

--- main.c
+++ main.c
@@ -14,41 +14,59 @@ Lista * criar(){
 return NULL;
 }
 
-Lista * insere (Lista *lista, char valor){
+Lista * insere (Lista * inicio,char r[],int size){
 
-Lista * novo = (Lista *) malloc(sizeof(Lista));
-novo->resposta[50] = valor;
-novo->prox = (struct Lista *) lista;
-return novo;
+ Lista *novo;
+ novo = (Lista *) malloc(sizeof(Lista));
+ for(int i = 0;i<size;i++){
+  novo->resposta[i] = r[i];
 }
-
-// Main do Projeto 
+ if(inicio == NULL){
+   novo->prox = NULL;
+   return novo;
+ }else{
+   Lista *pAux = inicio;
+   
+   while(pAux->prox != NULL){
+     pAux = pAux->prox;
+   }  
+    novo->prox = NULL;
+    pAux->prox = novo;
+ 
+    return inicio;
+   }  
+}
 
 void apresentar(Lista * lista){
   Lista * p;
-  p=lista;
-  while(p!=NULL){
-    printf("Valor: %s\n",&p->resposta[50]);
-    p = (Lista *) p->prox;
-  }  
+  p = lista;
+  do{
+    printf("Valor: %s\n",p->resposta);
+    p = p->prox;
+  }while(p!=NULL); 
 }
+// Main do Projeto
 
 int main(void) {
-	char *perguntas[5] = {"Qual o seu nome?", "Você conhece os protudos da Empresa?", "Como você avalia o atendimento prestado pela nossa equipe durante a compra de alguns dos produtos?", "Os nossos produtos estão abaixo, acima, ou atende às suas expectativas?","Em uma escala de 0 a 10, o quanto você nos recomendaria para um amigo ou familiar?"};
+
+  char *perguntas[5] = {"Qual o seu nome?", "Você conhece os protudos da Empresa?", "Como você avalia o atendimento prestado pela nossa equipe durante a compra de alguns dos produtos?", "Os nossos produtos estão abaixo, acima, ou atende às suas expectativas?","Em uma escala de 0 a 10, o quanto você nos recomendaria para um amigo ou familiar?"};
+
   Lista *lista;
-  char valor[50];
-	
+  char RecebeResposta[50];
+
 	printf("Ola, a empresa Start esta passando por melhorias e precisa da sua opiniao!\n");
 	printf("Por favor, responda as seguintes perguntas com o maximo de sinseridade possivel.\n");
 
   lista = criar();
-
+  
   for(int i = 0; i <= 4; i++){
     printf("%s\n", perguntas[i]);
-    scanf("%s",&valor[50]);
-    lista = insere(lista,*valor);
+    fgets(RecebeResposta,50,stdin);
+    lista = insere(lista,RecebeResposta,50);
   }
+ 
+ apresentar(lista);
 
-  apresentar(lista);
-  return 0;
+
+return 0;
 }
